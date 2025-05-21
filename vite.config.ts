d18ab391ae111604f5bd51defb68f7b416958a9b
HabaneraBi <beriya2016@mail.ru>
2025-05-21 13:48:50 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/children_holiday/",
+  base: "/children-holiday/",
   plugins: [react(), tailwindcss()],
 });
